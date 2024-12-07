c3b5f410f79508be41eb0444dc3a018de1ce26d3
Masato Miura <hi@masato.io>
2024-12-06 18:18:03 -0800

    f

--- apps/frontend/main-public.tf
+++ apps/frontend/main-public.tf
@@ -1,5 +1,5 @@
 module "vercel_deployment" {
-  source = "https://github.com/masato-io/omakase/tree/main/apps/frontend"
+  source = "github.com/masato-io/omakase/apps/frontend"
   project_name = var.project_name
   vercel_api_token = var.vercel_api_token
 }
